0e1ef554551ff8111155197a3e1964d05fc0a355
Cody Chand <chandcody@gmail.com>
2020-03-03 11:41:49 -0800

    removed node_modules from vertical folder

--- vertical/node_modules/metismenujs/dist/types/constant.d.ts
+++
@@ -1,4 +0,0 @@
-import { IMMClassNames, IMMOptions } from "./interface";
-export declare type MetisMenuEvents = "show.metisMenu" | "shown.metisMenu" | "hide.metisMenu" | "hidden.metisMenu";
-export declare const Default: IMMOptions;
-export declare const ClassName: IMMClassNames;
